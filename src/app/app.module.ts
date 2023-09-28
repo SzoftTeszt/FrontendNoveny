68342995a3a401ee32c078178255add42fdca5e5
JagerAttila <kitalalt@gmail.com>
2023-09-28 12:17:04 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RolunkComponent } from './part/rolunk/rolunk.component';
+import { NavComponent } from './part/nav/nav.component';
+import { Enviroments } from './enviroments';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RolunkComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    AngularFireModule.initializeApp(Enviroments.firebaseConfig),
+    AngularFireDatabaseModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
